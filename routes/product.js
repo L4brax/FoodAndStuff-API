0ac1992c4f9880d024362dc67717f87915c2935e
AlexLP <alexlepoupon@gmail.com>
2018-05-24 10:57:48 +0200

    modif product route for find all product

--- routes/product.js
+++ routes/product.js
@@ -3,27 +3,6 @@ var router = express.Router();
 
 const Product = require('.././models/Product');
 
-var MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
-// var userpwd = "userFS";
-// var password = "ynovnantes18";
-const dbName = 'foodandstuff';
-
-// Connection URL
-// const url = 'mongodb://35.178.103.130:23992';
-const url2 = 'mongodb://userFS:ynovnantes18@35.178.103.130:23992/foodandstuff';
-
-const findProduct = function(db, callback) {
-  // Get the product collection
-  const collection = db.collection('Product');
-  // Find some products
-  collection.find({}).toArray(function(err, docs) {
-    assert.equal(err, null);
-    callback(docs);
-  });
-};
-
 router.get('/', function(req, res){
   res.json({
     message : 'Route product',
@@ -31,26 +10,17 @@ router.get('/', function(req, res){
   });
 });
 
-router.get('/list', function(req, res){
-  // Use connect method to connect to the server
-  MongoClient.connect(url2, function(err, client) {
-    assert.equal(null, err);
-    const db = client.db(dbName);
-    findProduct(db, function(data) {
-      res.json({
-        message : data,
-        status : 'ok'
-      });
-      client.close();
-    });
+// Find a product by id
+router.get('/id/:product_id', function (req, res) {
+  Product.findOne({ product_id: req.params.product_id }, function(err, product) {
+    if (err) res.send(err);
+    res.send(product);
   });
-
 });
 
-
-// Find a product by id
-router.get('/:product_id', function (req, res) {
-  Product.findOne({ product_id: req.params.product_id }, function(err, product) {
+// Find all product
+router.get('/list', function(req, res) {
+  Product.find({ }, function(err, product) {
     if (err) res.send(err);
     res.send(product);
   });
